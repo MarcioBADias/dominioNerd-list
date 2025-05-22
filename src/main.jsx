4a14b7eb5013192a39e0600e64af3de3da335f76
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-05-22 01:25:45 -0300

    ⚠️ Ajuste no caminho da Home

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "@/app.jsx";
-import "@/index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from '@/app.jsx'
+import { GlobalStyle } from '@/styles/GlobalStyle '
 
-const rootElement = document.querySelector('[data-js="root"]');
-const root = createRoot(rootElement);
+const rootElement = document.querySelector('[data-js="root"]')
+const root = createRoot(rootElement)
 
 root.render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>,
-);
+)
