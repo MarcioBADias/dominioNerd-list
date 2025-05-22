ab29a7391093b367cdb9111f4e27c5caf79733b5
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-05-22 01:29:28 -0300

    ⚠️ Ajuste no caminho da Home

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 })
