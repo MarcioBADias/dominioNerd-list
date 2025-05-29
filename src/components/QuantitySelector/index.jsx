af8ed8cc722c68c9b98c8b58c30a5e3a60aaf080
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-05-28 22:59:43 -0300

    ⚠️ Ajuste no checkout colocando valores nos itens e melhorando a descrição.

--- src/components/QuantitySelector/index.jsx
+++ src/components/QuantitySelector/index.jsx
@@ -1,3 +1,5 @@
+// QuantitySelector.jsx
+
 import { useState } from 'react'
 
 const QuantitySelector = ({ items, onSubmit, onCancel }) => {
@@ -14,12 +16,21 @@ const QuantitySelector = ({ items, onSubmit, onCancel }) => {
     setQuantities({ ...quantities, [itemId]: newValue })
   }
 
+  // Novo cálculo do valor total do pedido
+  const totalPrice = items.reduce((acc, item) => {
+    const selectedQuantity = quantities[item.id] || 0 // Garante que seja 0 se não houver quantidade
+    return acc + item.price * selectedQuantity
+  }, 0)
+
   return (
     <div style={{ background: '#fff', padding: 20 }}>
-      <h2>Definir quantidade do pedido</h2>
+      <h2 style={{ marginBottom: 20 }}>Definir quantidade do pedido</h2>
       {items.map((item) => (
         <div key={item.id}>
-          <strong>{item.name}</strong>
+          <strong>
+            {item.edition} {item.serialNumber} - {item.name}
+          </strong>
+          <span> Preço: R$ {item.price},00</span>
           <div>
             <button
               style={{
@@ -57,8 +68,17 @@ const QuantitySelector = ({ items, onSubmit, onCancel }) => {
             </button>
             <span> {item.quantity} disponíveis</span>
           </div>
+          <div style={{ marginBottom: 10 }}>
+            {/* Subtotal por item - corrigido para usar a quantidade selecionada */}
+            <strong>
+              Subtotal: R$ {item.price * (quantities[item.id] || 0)},00
+            </strong>
+          </div>
         </div>
       ))}
+      <br />
+      {/* Exibição do valor final do pedido */}
+      <h2>Valor final do pedido: R$ {totalPrice},00</h2>
       <br />
       <button
         style={{
